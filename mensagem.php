5199777682cad132ec6ea784ae04b6e73866ce30
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-14 16:16:54 -0300

    Adding PHP Mailer library

--- mensagem.php
+++ mensagem.php
@@ -1,20 +1,33 @@
 <?php
 
-class Mensagem {
+require './bibliotecas/PHPMailer/Exception.php';
+require './bibliotecas/PHPMailer/OAuth.php';
+require './bibliotecas/PHPMailer/PHPMailer.php';
+require './bibliotecas/PHPMailer/POP3.php';
+require './bibliotecas/PHPMailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mensagem
+{
     private $para = null;
     private $assunto = null;
     private $mensagem = null;
 
-    public function __get($atributo){
+    public function __get($atributo)
+    {
         return $this->$atributo;
     }
-    
-    public function __set($atributo, $valor){
+
+    public function __set($atributo, $valor)
+    {
         $this->$atributo = $valor;
     }
 
-    public function mensagemValida() {
-        if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+    public function mensagemValida()
+    {
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
             return false;
         }
 
@@ -30,8 +43,44 @@ $mensagem->__set('para', $_POST['para']);
 $mensagem->__set('assunto', $_POST['assunto']);
 $mensagem->__set('mensagem', $_POST['mensagem']);
 
-if($mensagem->mensagemValida()) {
+if (!$mensagem->mensagemValida()) {
     echo 'mensagem valida';
-} else {
-    echo 'mensagem invalida';
+    die();
+}
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.example.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'user@example.com';
+    $mail->Password = 'secret';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');
+    $mail->addAddress('ellen@example.com');
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Here is the subject';
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Não foi possível enviar este e-mai. Error: {$mail->ErrorInfo}";
 }
